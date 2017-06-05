c39b3b26354a5c7a4fb53c8c93836a500b243644
crash <dima@pindrop.nl>
2017-06-06 00:35:52 +0300

    init

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
 import { Router, ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 
 
 @Component({
@@ -9,9 +10,8 @@ import { Router, ActivatedRoute } from "@angular/router";
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-
-
+export class RecipeListComponent implements OnInit, OnDestroy {
+    subscription: Subscription;
     recipes: Recipe[];
 
     constructor(private recipeService: RecipeService,
@@ -21,6 +21,16 @@ export class RecipeListComponent implements OnInit {
 
     ngOnInit(){
         this.recipes = this.recipeService.getRecipes();
+        this.subscription = this.recipeService.recipesChanged
+            .subscribe(
+                (recipes: Recipe[]) => {
+                    this.recipes = recipes;
+                }
+            );
+    }
+
+    ngOnDestroy(){
+        this.subscription.unsubscribe();
     }
 
     addNew(){
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,29 +1,65 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Ingredient } from "../../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list.service";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-shopping-edit',
     templateUrl: './shopping-edit.component.html',
     styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-    @ViewChild('nameInput') nameInputRef: ElementRef;
-    @ViewChild('amountInput') amountInputRef: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+    @ViewChild('f') slForm: NgForm;
+    subscription: Subscription;
+    editMode = false;
+    editedItemIndex: number;
+    editedItem: Ingredient;
 
     constructor(private shoppingListService: ShoppingListService){
     }
 
     ngOnInit(){
+        this.subscription = this.shoppingListService.startedEditing.subscribe(
+            (index: number) =>{
+                this.editMode = true;
+                this.editedItemIndex = index;
+                this.editedItem = this.shoppingListService.getIngredient(index);
+                this.slForm.setValue({
+                    name: this.editedItem.name,
+                    amount: this.editedItem.amount
+                });
+            }
+        )
     }
 
-    onAddItem(){
-        this.shoppingListService.addIngredient(
-            new Ingredient(
-                this.nameInputRef.nativeElement.value,
-                this.amountInputRef.nativeElement.value
-            )
-        );
+    ngOnDestroy(){
+        this.subscription.unsubscribe();
     }
+
+    onSubmit(form: NgForm){
+        const value = form.value;
+        const newIngredient = new Ingredient(value.name, value.amount);
+
+        if (this.editMode) {
+            this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+            this.editMode = false;
+        } else {
+            this.shoppingListService.addIngredient(newIngredient);
+        }
+
+        this.slForm.reset();
+    }
+
+    onClear(){
+        this.slForm.reset();
+        this.editMode = false;
+    }
+
+    onDelete(){
+        this.shoppingListService.deleteIngredient(this.editedItemIndex);
+        this.onClear();
+    }
+
 
 }
